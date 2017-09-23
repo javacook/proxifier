bc5759a98ee098c7eb16d3030f8e17d34c5167dc
javacook <javacook@gmx.de>
2017-09-23 23:08:33 +0200

    Beautyfied

--- src/test/java/com/javacook/proxifier/ProxifierMain.java
+++ src/test/java/com/javacook/proxifier/ProxifierMain.java
@@ -5,13 +5,16 @@ public class ProxifierMain {
     public static void main(String[] args) {
         final Dog dog = new Dog();
         final Hund hund = new Hund("Pudel", 23, true);
-        final Dog dogProxy = Proxifier.proxyOf(dog);
-        final Hund hundProxy = Proxifier.proxyOf(hund);
-        dogProxy.setRace(hundProxy.getRasse());
-        dogProxy.setWeight(hundProxy.getGewicht());
-        dogProxy.setNice(hundProxy.isLieb());
-        Proxifier.assertAllSettersInvoked(dogProxy, "setNice");
-        Proxifier.assertAllGettersInvoked(hundProxy, "isLieb");
+
+        final Dog dogPx = Proxifier.proxyOf(dog);
+        final Hund hundPx = Proxifier.proxyOf(hund);
+
+        dogPx.setRace(hundPx.getRasse());
+        dogPx.setWeight(hundPx.getGewicht());
+        dogPx.setNice(hundPx.isLieb());
+
+        Proxifier.assertAllSettersInvoked(dogPx);
+        Proxifier.assertAllGettersInvoked(hundPx);
 
         System.out.println(dog);
     }
